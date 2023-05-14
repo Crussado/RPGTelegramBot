2d182f4ea4c718615efd0808c73519f5ed12f08e
Julio <julioolivencia891@gmail.com>
2023-05-13 23:30:20 -0300

    constants

---
+++ settings.py
@@ -0,0 +1,6 @@
+CLASSES = ['Rogue', 'Wizzard', 'Barbarian', 'Bardo', 'Archer']
+RACES = ['Human', 'Elf', 'Orc', 'Dragonborn', 'Dwarf', 'Gnome']
+
+STATS = ['str', 'dex', 'res', 'int']
+
+ENEMY_KEYS = ['nombre', 'aspecto', 'situacion', 'tesoro']
